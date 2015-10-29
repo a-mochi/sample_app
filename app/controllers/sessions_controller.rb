95eed83cfd9b9049d128482776162c633eb1a6b1
a-mochi <mochizuki@val.co.jp>
2015-10-29 20:23:35 +0900

    できてない

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,6 +6,8 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
       #ユーザーをサインインさせ、ユーザーページ(show)にリダイレクトする。
+      sign_in user
+      redirect_to user
     else
       flash.now[:error] = 'Invalid email/password combination' #謝りあり！
     render 'new'
